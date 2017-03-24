b8dfbc56aba2351010b47086618b4954c1deaf77
brennanMKE <brennan@smallsharptools.com>
2017-03-24 11:13:14 -0700

    adds more projects to try to recreate linking problem

---
+++ NothingKit/NothingKit.h
@@ -0,0 +1,19 @@
+//
+//  NothingKit.h
+//  NothingKit
+//
+//  Created by Brennan Stehling on 3/23/17.
+//  Copyright © 2017 SmallSharpTools LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NothingKit.
+FOUNDATION_EXPORT double NothingKitVersionNumber;
+
+//! Project version string for NothingKit.
+FOUNDATION_EXPORT const unsigned char NothingKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NothingKit/PublicHeader.h>
+
+
---
+++ SomethingKit/SomethingKit.h
@@ -0,0 +1,19 @@
+//
+//  SomethingKit.h
+//  SomethingKit
+//
+//  Created by Brennan Stehling on 3/23/17.
+//  Copyright © 2017 SmallSharpTools LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SomethingKit.
+FOUNDATION_EXPORT double SomethingKitVersionNumber;
+
+//! Project version string for SomethingKit.
+FOUNDATION_EXPORT const unsigned char SomethingKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SomethingKit/PublicHeader.h>
+
+
